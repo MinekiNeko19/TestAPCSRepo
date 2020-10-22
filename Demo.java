e14e2437501e87345abb0f962b899e0eabae384d
Sarah Leong <sleong20@stuy.edu>
2020-10-22 16:20:51 -0400

    Added and tested create2DArray method

--- Demo.java
+++ Demo.java
@@ -7,17 +7,19 @@ public class Demo {
     // }
 
     //10-22-20
-    int[] a = {0,3,2};
-    int[] b = {};
-    int[] c = {1238013};
-    int[] d = {54,3512,4672,4,5,6,2};
-    int[] e = {7,2,9,3,3,423};
-    int[][] f = {a,b,c,d,e};
-    System.out.println(arrToString(a));
-    // for (int i = 0; i < f.length; i++) {
-    //   System.out.println(arrToString(f[i]));
-    // }
-    System.out.print(arrayDeepToString(f));
+    // int[] a = {0,3,2};
+    // int[] b = {};
+    // int[] c = {1238013};
+    // int[] d = {54,3512,4672,4,5,6,2};
+    // int[] e = {7,2,9,3,3,423};
+    // int[][] f = {a,b,c,d,e};
+    // System.out.println(arrToString(a));
+    // // for (int i = 0; i < f.length; i++) {
+    // //   System.out.println(arrToString(f[i]));
+    // // }
+    // System.out.print(arrayDeepToString(f));
+
+    create2DArray(5, 4, 4);
   }
 
   public static void printLoop(int n){
@@ -54,5 +56,16 @@ public class Demo {
     return ans;
   }
   
-  
+    public static int[][] create2DArray(int rows, int cols, int maxValue) {
+      int[][] ans = new int[rows][cols];
+      for (int i = 0; i < ans.length;i++) {
+        for (int j = 0; j < ans[i].length; j++) {
+          ans[i][j] = (int)(Math.random() * (maxValue+1));
+        }
+      }
+
+      System.out.print(arrayDeepToString(ans));
+
+      return ans;
+    }
 }
